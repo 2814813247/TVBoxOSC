e26c3cc996c04b1f2a087018622adc6530ee2a07
2814813247 <78680132+2814813247@users.noreply.github.com>
2024-04-01 23:09:35 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://fmstar.cn/ipa/1227.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
